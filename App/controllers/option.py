b07a0153560ef1aa9de42d9d11cdd963d01e5529
DaNJO-Y <daniel.young@my.uwi.edu>
2025-03-17 14:19:33 +0000

    Created Model for Option Added attributes for Option model Added controllers for option

---
+++ App/controllers/option.py
@@ -0,0 +1,47 @@
+from App.models import Option
+from App.database import db
+
+def create_option(question_id):
+    new_option = Option(questionId = question_id)
+    db.session.add(new_option)
+    db.session.commit()
+    return new_option
+
+def get_option(id):
+    return Option.query.get(id)
+
+def add_text(id, text, question_id):
+    option = get_option(id)
+    if option and option.question_id == question_id:
+        option.body = text
+        db.session.add(option)
+        db.session.commit()
+        return option
+    return None
+
+def add_image(id, image, question_id):
+    option = get_option(id)
+    if option and option.question_id == question_id:
+        option.image = image
+        db.session.add(option)
+        db.session.commit()
+        return option
+    return None
+
+def remove_text(id, question_id):
+    option = get_option(id)
+    if option and option.questionId == question_id:
+        option.body = None  # Set body to None to remove text
+        db.session.add(option)
+        db.session.commit()
+        return option
+    return None
+
+def remove_image(id, question_id):
+    option = get_option(id)
+    if option and option.questionId == question_id:
+        option.image = None  # Set image to None to remove image
+        db.session.add(option)
+        db.session.commit()
+        return option
+    return None
