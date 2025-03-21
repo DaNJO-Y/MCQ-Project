52eef69dc9bb4fb124f05596944542fd2022463b
Monique Britto <82354847+moniquebritto21@users.noreply.github.com>
2025-03-21 11:49:52 +0000

    Errors saving for now to new branch still editing... Bridge table is not working getting errors referencing foreign key for tag

--- App/models/question.py
+++ App/models/question.py
@@ -1,24 +1,22 @@
 from .teacher import Teacher
 from .exam import Exam
-from .tag import Tag
+from .associations import Question_Tag_Bridge
 from App.database import db
 
 class Question(db.Model):
-    __tablename__ = 'question'
+    __tablename__ = 'question'   
     id = db.Column(db.Integer, primary_key=True)
     teacherId = db.Column(db.Integer, db.ForeignKey('teacher.id'))
     examId = db.Column(db.Integer, db.ForeignKey('exam.id'))
-    # tagId = db.Column(db.Integer, db.ForeignKey('tag.id'))
-    correctOption = db.relationship('Option', backref=db.backref('question_correct_option', lazy='joined'))
-    # teacher = db.relationship('Teacher', backref=db.backref('question', lazy='joined'))
+    correctOption = db.relationship('Option', backref=db.backref('question_correct_option', lazy='joined'))#where is question_correct_option?
     image = db.Column(db.String(300))
     difficulty = db.Column(db.String(200))
-    tag = db.relationship('Tag', secondary='question_tag_bridge', back_populates = 'question')
+    tag = db.relationship('Tag', secondary='question_tag_bridge', back_populates='question')
     courseCode = db.Column(db.String(200))
-    exams = db.relationship('Exam', secondary='exam_question', back_populates='questions', lazy='joined')
-    statistics = db.relationship('QuestionStatistics', backref=db.backref('question_stats',lazy='joined'))
-    options = db.relationship('Option', back_populates = 'question')
-    lastUsed = db.Column(db.Date, nullable = True)
+    exams = db.relationship('Exam', secondary='exam_question', back_populates='question', lazy='joined')
+    statistics = db.relationship('QuestionStatistics', backref=db.backref('question_statistics', lazy='joined'))
+    options = db.relationship('Option', back_populates='question')
+    lastUsed = db.Column(db.Date, nullable=True)
 
     def __init__(self, teacherId, correctOption, difficulty, courseCode, options):
         self.teacherId = teacherId
@@ -31,9 +29,8 @@ class Question(db.Model):
         return {
             "id": self.id,
             "teacherid": self.teacherId,
-            "correct option":self.correctOption,
+            "correct option": self.correctOption,
             "difficulty": self.difficulty,
-            "course code":self.courseCode,
-            "options":[options for op in self.options]
+            "course code": self.courseCode,
+            "options": [options for op in self.options]
         }
-
