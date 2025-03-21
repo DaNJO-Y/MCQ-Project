9316f01f9fdbf4b52d44e0c06f1ee43b340bba49
DaNJO-Y <122925211+DaNJO-Y@users.noreply.github.com>
2025-03-21 08:04:05 -0400

    Revert "Errors saving for now to new branch still editing..."
    This reverts commit 52eef69dc9bb4fb124f05596944542fd2022463b.

--- App/models/question.py
+++ App/models/question.py
@@ -1,22 +1,24 @@
 from .teacher import Teacher
 from .exam import Exam
-from .associations import Question_Tag_Bridge
+from .tag import Tag
 from App.database import db
 
 class Question(db.Model):
-    __tablename__ = 'question'   
+    __tablename__ = 'question'
     id = db.Column(db.Integer, primary_key=True)
     teacherId = db.Column(db.Integer, db.ForeignKey('teacher.id'))
     examId = db.Column(db.Integer, db.ForeignKey('exam.id'))
-    correctOption = db.relationship('Option', backref=db.backref('question_correct_option', lazy='joined'))#where is question_correct_option?
+    # tagId = db.Column(db.Integer, db.ForeignKey('tag.id'))
+    correctOption = db.relationship('Option', backref=db.backref('question_correct_option', lazy='joined'))
+    # teacher = db.relationship('Teacher', backref=db.backref('question', lazy='joined'))
     image = db.Column(db.String(300))
     difficulty = db.Column(db.String(200))
-    tag = db.relationship('Tag', secondary='question_tag_bridge', back_populates='question')
+    tag = db.relationship('Tag', secondary='question_tag_bridge', back_populates = 'question')
     courseCode = db.Column(db.String(200))
-    exams = db.relationship('Exam', secondary='exam_question', back_populates='question', lazy='joined')
-    statistics = db.relationship('QuestionStatistics', backref=db.backref('question_statistics', lazy='joined'))
-    options = db.relationship('Option', back_populates='question')
-    lastUsed = db.Column(db.Date, nullable=True)
+    exams = db.relationship('Exam', secondary='exam_question', back_populates='questions', lazy='joined')
+    statistics = db.relationship('QuestionStatistics', backref=db.backref('question_stats',lazy='joined'))
+    options = db.relationship('Option', back_populates = 'question')
+    lastUsed = db.Column(db.Date, nullable = True)
 
     def __init__(self, teacherId, correctOption, difficulty, courseCode, options):
         self.teacherId = teacherId
@@ -29,8 +31,9 @@ class Question(db.Model):
         return {
             "id": self.id,
             "teacherid": self.teacherId,
-            "correct option": self.correctOption,
+            "correct option":self.correctOption,
             "difficulty": self.difficulty,
-            "course code": self.courseCode,
-            "options": [options for op in self.options]
+            "course code":self.courseCode,
+            "options":[options for op in self.options]
         }
+
--- App/models/tag.py
+++ App/models/tag.py
@@ -1,12 +1,16 @@
 #Monique Britto
-
-from sqlalchemy import Table, Column, Integer, ForeignKey
 from App.database import db
 from flask_login import UserMixin
-from .associations import Question_Tag_Bridge
+
+Question_Tag_Bridge= Table('question_tag_bridge',
+        Column('id',Integer,primary_key=True),
+        Column('tag_id',Integer, ForeignKey(tag.id)),
+        Column(question_id),Integer,ForeignKey(question.id))
+
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
-    tag_text = db.Column(db.String(120), nullable=False)
-    question = db.relationship('Question', secondary='question_tag_bridge', back_populates='tag')
+    tag_text = db.Column(db.String(120), nullable = False)
+    question = db.relationship('Question', secondary='Question_Tag_Bridge', backref=db.backref('tag', lazy=True))
+
