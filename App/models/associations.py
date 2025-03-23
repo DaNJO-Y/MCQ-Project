605ac9ba4bf0acb029dd239fc70a7b9f63d5d23e
Monique Britto <82354847+moniquebritto21@users.noreply.github.com>
2025-03-23 15:04:31 +0000

    added controller for adding a new tag

--- App/models/associations.py
+++ App/models/associations.py
@@ -2,15 +2,18 @@
 
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from App.database import db
-# from .question import Question
-# from .tag import Tag
 from flask_login import UserMixin
 
 
 Question_Tag_Bridge = Table(
     'question_tag_bridge',
-    db.metadata,  # Important for SQLAlchemy to register it
+    db.metadata, 
     Column('id', Integer, primary_key=True),
     Column('tag_id', Integer, ForeignKey('tag.id')),
     Column('question_id', Integer, ForeignKey('question.id'))
 )
+
+
+def __init__(self, tag_id, question_id):
+        self.tag_id = tag_id
+        self.question_id = question_id
