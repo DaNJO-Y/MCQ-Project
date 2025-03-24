81d902b5d40999922762d7d1480beb64ae007a94
gShah2001 <97922672+gShah2001@users.noreply.github.com>
2025-03-24 18:29:49 +0000

    Directed the My Exams button in the side nav to the My Exams page

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,8 +5,8 @@ from .index import index_views
 from .auth import auth_views
 from .questions import questions_views
 from .admin import setup_admin
+from .exams import exams_views
 
 
-
-views = [user_views, index_views, auth_views,questions_views] 
+views = [user_views, index_views, auth_views,questions_views, exams_views] 
 # blueprints must be added to this list
