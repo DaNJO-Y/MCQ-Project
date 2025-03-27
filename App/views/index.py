17a16de8f5a74c72cfa7f82219fcea3908154165
DaNJO-Y <daniel.young@my.uwi.edu>
2025-03-27 19:04:39 +0000

    Made a temporary fix to allow createion of all database columns upon each run of application

--- App/views/index.py
+++ App/views/index.py
@@ -5,7 +5,8 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    # initialize()
+    # initialized will be commented off when in production to prevent loss of data
+    initialize()
     return render_template('login.html')
 
 @index_views.route('/init', methods=['GET'])
