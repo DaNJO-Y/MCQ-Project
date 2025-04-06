8be90fad55b99490504b68d28b8dea8e89ce077a
DaNJO-Y <daniel.young@my.uwi.edu>
2025-04-06 12:50:09 +0000

    made changes to env variables

--- App/config.py
+++ App/config.py
@@ -1,7 +1,9 @@
 import os
 # from flask import Flask
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
 
+load_dotenv()
 def load_config(app, overrides):
     if os.path.exists(os.path.join('./App', 'custom_config.py')):
         app.config.from_object('App.custom_config')
@@ -17,15 +19,19 @@ def load_config(app, overrides):
     app.config["JWT_COOKIE_SECURE"] = True
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
-    app.config['SENDGRID_API_KEY'] = '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'
+    # app.config['SENDGRID_API_KEY'] = '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'
+    app.config['SENDGRID_API_KEY'] = os.getenv('SENDGRID_API_KEY')
     app.config['MAIL_SERVER']= 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = 'd4884781@gmail.com'  # Replace with your Gmail address
-    app.config['MAIL_PASSWORD'] = 'hxny nokz pgjr txrq'      # Replace with your Gmail password or App Password
+    # app.config['MAIL_USERNAME'] = 'd4884781@gmail.com'  # Replace with your Gmail address
+    # app.config['MAIL_PASSWORD'] = 'hxny nokz pgjr txrq'      # Replace with your Gmail password or App Password
     app.config['MAIL_DEFAULT_SENDER'] = 'd4884781@gmail.com'
-    app.config['SECRET_KEY'] = 'c?|CVNoxkYqfB}|o$A6G'
+    # app.config['SECRET_KEY'] = 'c?|CVNoxkYqfB}|o$A6G'
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     mail = Mail(app)
    
     for key in overrides:
