faf326d7c6d8c4864c55dbafcfc34e143203b638
DaNJO-Y <122925211+DaNJO-Y@users.noreply.github.com>
2025-04-06 04:02:48 -0400

    Revert "Revert "Create exam""

--- App/models/question_statistics.py
+++ App/models/question_statistics.py
@@ -1,15 +1,15 @@
 from .question import Question
 from App.database import db
 
-class 	QuestionStatistics(db.Model):
+class QuestionStatistics(db.Model):
     __tablename__ = 'question_statistics'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
     data = db.Column(db.String(255), nullable=False)
     year =db.Column(db.Integer, nullable=False)
 
-    question = db.relationship('Question', backref=db.backref('question_statistics',lazy='joined'))
+    question = db.relationship('Question', back_populates='statistics') # Use back_populates
 
     def __init__(self, question_id, data, year):
         self.question_id = question_id
@@ -20,5 +20,5 @@ class 	QuestionStatistics(db.Model):
         return {
             "id": self.id,
             "question_id": self.question_id,
-            "year": self.year 
+            "year": self.year
         }
